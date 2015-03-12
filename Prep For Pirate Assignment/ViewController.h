f1ecce0630f84f45c53204e600eaa85a41ce5e20
Jim Veneskey <github@strifecrafter.com>
2015-03-12 12:31:03 -0400

    Initial Commit

---
+++ Prep For Pirate Assignment/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Prep For Pirate Assignment
+//
+//  Created by jim Veneskey on 3/12/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Prep For Pirate AssignmentTests/Prep_For_Pirate_AssignmentTests.m
@@ -0,0 +1,34 @@
+//
+//  Prep_For_Pirate_AssignmentTests.m
+//  Prep For Pirate AssignmentTests
+//
+//  Created by jim Veneskey on 3/12/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Prep_For_Pirate_AssignmentTests : XCTestCase
+
+@end
+
+@implementation Prep_For_Pirate_AssignmentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
